d8f7acbc123a454b92efb28ca22c379e132cfe8e
Elijah <atolagbe.elijah.21@gmail.com>
2022-11-28 08:24:28 +0100

    add note batching functionality for revision fragment

--- app/src/main/java/com/example/simplereader_v1/ui/RevisionFragment.kt
+++ app/src/main/java/com/example/simplereader_v1/ui/RevisionFragment.kt
@@ -4,7 +4,6 @@ import android.app.Dialog
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
@@ -13,9 +12,9 @@ import com.example.simplereader_v1.Adapters.RevisonFragmentViewPager2Adapter
 import com.example.simplereader_v1.MyApplication
 import com.example.simplereader_v1.R
 import com.example.simplereader_v1.ReaderActivity
+import com.example.simplereader_v1.database.models.Note
 import com.example.simplereader_v1.viewModels.LibraryBooksAndNotesViewModel
 import com.example.simplereader_v1.viewModels.LibraryBooksAndNotesViewModelFactory
-import com.pspdfkit.internal.va
 
 class RevisionFragment : DialogFragment() {
     val viewModel : LibraryBooksAndNotesViewModel by activityViewModels{
@@ -26,17 +25,55 @@ class RevisionFragment : DialogFragment() {
     }
     lateinit var readerActivity: ReaderActivity
 
+    var noteBatchCount = 0;
+    var maxBatchNo = 0;
+    //Current batch number
+    var batchCountNo = 0;
+    lateinit var notes : List<Note>
+    lateinit var revisionNotes: List<Note>
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val revisionNotes = viewModel.getRevisionNote(readerActivity.bookName)
+        notes = viewModel.getNotes(readerActivity.bookName).filter {
+            !it.text.equals("")
+        }
+        maxBatchNo = viewModel.getLastBatch(notes)
+        batchCountNo = maxBatchNo
+
+
+        revisionNotes = notes.filter { it.batchNo == batchCountNo }
         val builder = AlertDialog.Builder(requireActivity())
         val view = layoutInflater.inflate(R.layout.fragment_revision,null)
         val myViewPager : ViewPager2 = view.findViewById<ViewPager2>(R.id.my_view_pager)
         val closeRevision: ImageView = view.findViewById(R.id.close_revision)
         val prevNote : ImageView =  view.findViewById(R.id.prev_note_icon)
         val nextNote : ImageView =  view.findViewById(R.id.next_note_icon)
+        val batchNoText : TextView = view.findViewById(R.id.batch_no)
+        val nextBatch = view.findViewById<ImageView>(R.id.next_batch)
+        val prevBatch = view.findViewById<ImageView>(R.id.prev_batch)
+        nextBatch.setOnClickListener {
+            if(batchCountNo < maxBatchNo){
+                batchCountNo++
+                revisionNotes = notes.filter { it.batchNo == batchCountNo }
+                batchNoText.text = getString(R.string.get_batch_no,batchCountNo)
+                myViewPager.adapter = RevisonFragmentViewPager2Adapter(this,revisionNotes)
+            }
+        }
+        prevBatch.setOnClickListener {
+            if(batchCountNo > 1){
+                batchCountNo--
+                revisionNotes = notes.filter { it.batchNo == batchCountNo }
+                batchNoText.text = getString(R.string.get_batch_no,batchCountNo)
+                myViewPager.adapter = RevisonFragmentViewPager2Adapter(this,revisionNotes)
+            }
+        }
         prevNote.setOnClickListener(){
 
         }
+        nextNote.setOnClickListener {
+
+        }
+
+        batchNoText.text = getString(R.string.get_batch_no,maxBatchNo)
 
         myViewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
             override fun onPageScrolled(
